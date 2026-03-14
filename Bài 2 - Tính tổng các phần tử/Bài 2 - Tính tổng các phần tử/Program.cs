ba14a7fbb890dc3b7482a6148435944343a1abb3
Nguyen Do Huu Giang <nguyendohuugiang2006@gmail.com>
2026-03-14 11:12:25 +0700

    Bài 2 - Tính tổng các phần tử

---
+++ Bài 2 - Tính tổng các phần tử/Bài 2 - Tính tổng các phần tử/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace LythuyetCS
+{
+    class Bai2
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("\t");
+            Console.WriteLine("Họ và tên: Nguyễn Đỗ Hữu Giang - MSV: 2415053122312");
+            Console.WriteLine("Bài 2: Tính tổng các phần tử");
+            List<int> list = new List<int>();
+            int tg = 0;
+            Console.Write("Nhập số phần tử muốn có trong list: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nhập phần tử thứ {0} ", i + 1);
+                int a = int.Parse(Console.ReadLine());
+                list.Add(a);
+            }
+            Console.WriteLine("Phần tử trong mảng vừa nhập là: ");
+            foreach(int a in list)
+            {
+                Console.Write("{0} ", a);
+            }
+            Console.WriteLine("\t");
+            foreach(int a in list)
+            {
+                tg = tg + a;
+            }
+            Console.Write("Tổng các phần tử trong mảng vừa nhập: ");
+            Console.WriteLine(tg);
+            Console.ReadLine();
+        }
+    }
+}
