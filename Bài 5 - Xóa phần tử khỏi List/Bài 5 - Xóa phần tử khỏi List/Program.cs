87d1cf61e12bcfefc06df41945a8c1e1a749b0e5
Nguyen Do Huu Giang <nguyendohuugiang2006@gmail.com>
2026-03-17 20:18:31 +0700

    Nộp bài tập 5

---
+++ Bài 5 - Xóa phần tử khỏi List/Bài 5 - Xóa phần tử khỏi List/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LythuyesCS
+{
+    class Bai5
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            
+            Console.WriteLine("\t");
+            Console.WriteLine("Họ và tên: Nguyễn Đỗ Hữu Giang - Msv: 2415053122312");
+            Console.WriteLine("Bài 5: Xoá phần tử khỏi List");
+            List<String> list = new List<String>();
+            Console.Write("Nhập số lượng họ tên cần nhập: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Nhập tên sinh viên thứ {0}: ", i+1);
+                String hten = Console.ReadLine();
+                list.Add(hten);
+            }
+            Console.WriteLine("Tên sinh viên trong list: ");
+            foreach(string hten in list)
+            {
+                Console.WriteLine(hten);
+            }
+            Console.WriteLine("Nhập tên cần xoá: ");
+            string xoa = Console.ReadLine();
+            for (int i = list.Count - 1; i >= 0; i--) //Count dùng để đếm số lượng phần tử có trong list
+            {
+                if (list[i] == xoa)
+                {
+                    list.RemoveAt(i);
+                }
+            }
+            Console.WriteLine("List sau khi xoá: ");
+            foreach (string hten in list)
+            {
+                Console.WriteLine(hten);
+            }
+            Console.ReadLine();
+        }
+    }
+}
