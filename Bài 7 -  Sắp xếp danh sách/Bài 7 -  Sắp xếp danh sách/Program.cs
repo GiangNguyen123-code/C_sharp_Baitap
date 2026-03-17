3834afc178d85967fb64457504ef2a6f2c6ccb84
Nguyen Do Huu Giang <nguyendohuugiang2006@gmail.com>
2026-03-17 20:57:24 +0700

    Nộp bài tập 7

---
+++ Bài 7 -  Sắp xếp danh sách/Bài 7 -  Sắp xếp danh sách/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LythuyetCS
+{
+    class Bai7 
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("\t");
+            Console.WriteLine("Họ và tên: Nguyễn Đỗ Hữu Giang - MSV: 2415053122312");
+            Console.WriteLine("Bài 7: Sắp xếp danh sách ");
+            Console.Write("Nhập số lượng phần tử có trong list: ");
+            int n = int.Parse(Console.ReadLine());
+            List<int> List = new List<int>(n);
+            Random r = new Random();
+            Console.Write("{0} số được tạo bất kỳ: ", n);
+            for (int i = 0; i < n; i++)
+            {
+                List.Add(r.Next(100));
+            }
+            foreach (int i in List)
+            {
+                Console.Write(i + "  ");
+            }
+            Console.WriteLine("\t");
+            Console.Write("Danh sách sắp xếp tăng dần: ");
+            List.Sort();
+            foreach (int i in List)
+            {
+                Console.Write(i + "  ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
