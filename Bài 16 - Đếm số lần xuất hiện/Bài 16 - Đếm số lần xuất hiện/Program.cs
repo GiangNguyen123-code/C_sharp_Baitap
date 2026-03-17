a4eae9bf0914117170a4168ac93a64023a55d15b
Nguyen Do Huu Giang <nguyendohuugiang2006@gmail.com>
2026-03-17 21:08:02 +0700

    Bài 16: Đếm số lần xuất hiện

---
+++ Bài 16 - Đếm số lần xuất hiện/Bài 16 - Đếm số lần xuất hiện/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+namespace LythuyetCS
+{
+    class Bai16
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Họ và tên: Nguyễn Đỗ Hữu Giang - MSV: 2415053122312");
+            Console.WriteLine("Bài 16: Đếm số lần xuất hiện");
+            Dictionary<char, int> Dem = new Dictionary<char, int>();
+            Console.Write("Nhập chuỗi cần đếm ký tự: ");
+            String input = Console.ReadLine();
+            
+            foreach(char c in input)
+            {
+                if (Dem.ContainsKey(c))
+                {
+                    Dem[c]++;
+                }
+                else
+                {
+                    Dem[c] = 1;
+                }
+            }
+            Console.WriteLine("\nKết quả đếm: ");
+            foreach (var item in Dem)
+            {
+                Console.Write($"{item.Key}: {item.Value} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
